c01a2a1717cbfef24f9aa7722db126785231b3a1
Adam Herrmann <adamherrmann13@gmail.com>
2021-03-07 23:01:55 -0500

    adding container registry to infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 2.50"
     }
   }
@@ -15,8 +15,8 @@ terraform {
 }
 
 provider "azurerm" {
-    features {}
- }
+  features {}
+}
 
 
 resource "azurerm_resource_group" "state-rg" {
@@ -28,20 +28,33 @@ resource "azurerm_resource_group" "state-rg" {
 }
 
 resource "azurerm_storage_account" "state-sa" {
-  depends_on = [azurerm_resource_group.state-rg]
-  name = "rbsportstfstate"
-  resource_group_name = azurerm_resource_group.state-rg.name
-  location = "East US"
-  account_tier = "Standard"
+  depends_on               = [azurerm_resource_group.state-rg]
+  name                     = "rbsportstfstate"
+  resource_group_name      = azurerm_resource_group.state-rg.name
+  location                 = "East US"
+  account_tier             = "Standard"
   account_replication_type = "ZRS"
-   
+
   lifecycle {
     prevent_destroy = true
   }
 }
 
 resource "azurerm_storage_container" "core-container" {
-  depends_on = [azurerm_storage_account.state-sa]
+  depends_on           = [azurerm_storage_account.state-sa]
   name                 = "rbsports-tfstate-sc"
   storage_account_name = azurerm_storage_account.state-sa.name
+}
+
+resource "azurerm_resource_group" "acr-rg" {
+  name     = "rbsports-acr-rg"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "rbsportsacr"
+  resource_group_name = azurerm_resource_group.acr-rg.name
+  location            = azurerm_resource_group.acr-rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
